2226b1421dd62ada0e44afd7ffab305cd047fca4
hajime <hjmx3599@gmail.com>
2021-12-29 16:12:28 +0900

    feat:🚀 投稿一覧でデータを取得して表示させました

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+{{$user->name}}さんも投稿しましょう！
 @foreach ($incidentPosts as $incidentPost)
 <div class="container-fluid mt-20" style="margin-left:-10px;">
     <div class="row">
@@ -7,17 +8,17 @@
             <div class="card mb-4">
                 <div class="card-header">
                     <div class="media flex-wrap w-100 align-items-center">
-                        <div class="media-body ml-3"> ①件名
-                            <div class="text-muted small"> ②投稿者名</div>
+                        <div class="media-body ml-3"> {{$incidentPost->title}}
+                            <div class="text-muted small"> {{$incidentPost->user->name}}</div>
                         </div>
                         <div class="text-muted small ml-3">
                             <div>投稿日</div>
-                            <div><strong> ③投稿作成日 </strong> </div>
+                            <div><strong> {{$incidentPost->created_at->diffForHumans()}} </strong> </div>
                         </div>
                     </div>
                 </div>
                 <div class="card-body">
-                    <p> ④本文 </p>
+                    <p> {{$incidentPost->body}} </p>
                 </div>
             </div>
         </div>
