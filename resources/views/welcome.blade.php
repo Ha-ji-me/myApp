1a8a80c3fd1c99d3435b47c327fbbb71d4aaa461
hajime <hjmx3599@gmail.com>
2021-12-31 18:46:19 +0900

    ✏️ ドキュメントのみ変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    あ　か　ぎ
+                    あかぎmyApp
                 </div>
 
                 <div class="links">
