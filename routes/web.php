f72036c2ffa00e1db4a3af4f9d762c563454eefa
hajime <hjmx3599@gmail.com>
2022-01-07 15:42:44 +0900

    doc:✏️ コメントを追加

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+//投稿機能関連
 Route::resource('/incident-post', 'IncidentPostController');
 //以下７つは上記リソースと同意(URLは'/incident-post'の統一、ルート名は'incident-post.index'などになる)
 // Route::get('/post', 'PostController@index')->name('post.index');
@@ -29,9 +30,12 @@ Route::resource('/incident-post', 'IncidentPostController');
 // Route::patch('/post/{post}', 'PostController@update')->name('post.update');
 // Route::delete('/post/{post}', 'PostController@destroy')->name('post.destroy');
 
+//コメント機能
 Route::post('/incident-post/comment/store','CommentController@store')->name('comment.store');
+//自分の投稿のみ
 Route::get('/my-post','HomeController@myPost')->name('home.myPost');
+//自分のコメント記事のみ
 Route::get('/my-comment','HomeController@myComment')->name('home.myComment');
-
+//お問い合わせ関連
 Route::get('/contact/create','ContactController@create')->name('contact.create');
 Route::post('/contact/store','ContactController@store')->name('contact.store');
