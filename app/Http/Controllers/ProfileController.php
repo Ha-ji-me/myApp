88d934c21ef3a91631ac8e0b84d2ddcd64b398d1
hajime <hjmx3599@gmail.com>
2022-01-21 16:32:45 +0900

    メソッド追加(update)

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
@@ -21,5 +23,34 @@ class ProfileController extends Controller
         return view('profile.edit', compact('user'));
     }
 
+    public function update(User $user, Request $request)
+    {
+        $this->authorize('update',$user);
+
+
+        //バリデーション
+        $inputs = request()->validate([
+            'name' => 'required|max:255',
+            'email' => ['required','email','max:255', Rule::unique('users')->ignore($user->id)],
+            'avatar'=>'image|max:1024',
+            'password'=>'required|confirmed|max:255|min:8',
+            'password_confirmation'=>'required|same:password'
+        ]);
+
+        //データベースに保存
+        $inputs['password'] = Hash::make($inputs['password']);
+
+        //アバター保存
+        if(request('avatar'))
+        {
+            $name = request()->file('avatar')->getClientOriginalName();
+            $avatar = date('Ymd_His').'_'.$name;
+            request()->file('avatar')->storeAs('public/avatar',$avatar);
+            $inputs['avatar'] = $avatar;
+        }
+
+        $user->update($inputs);
+        return back()->with('message','情報を更新しました');
+    }
 
 }
