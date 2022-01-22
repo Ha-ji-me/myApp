80a1b3c77a28b4122b06d753e744291645237d9b
hajime <hjmx3599@gmail.com>
2022-01-22 13:59:06 +0900

    delete追加(ルーティング)

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,8 @@ Route::get('/profile/index','ProfileController@index')->name('profile.index');
 //プロフィール編集
 Route::get('/profile/{user}','ProfileController@edit')->name('profile.edit');
 Route::put('/profile/{user}','ProfileController@update')->name('profile.update');
+//管理者
+Route::middleware(['can:admin'])->group(function(){
+    Route::get('/profile/index', 'ProfileController@index')->name('profile.index');
+    Route::delete('/profile/delete/{user}', 'ProfileController@delete')->name('profile.delete');
+});
