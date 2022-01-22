ba9b4ee72c344691950bb525432bc6c17986bc23
hajime <hjmx3599@gmail.com>
2022-01-22 15:29:23 +0900

    ✏️ ドキュメントのみ変更

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -26,26 +26,26 @@
             <td>{{$user->email}}</td>
             <!-- アバター -->
             <td>
-            <img src="{{asset('storage/avatar/'.($user->avatar??'user_default.jpg'))}}"
-            class="rounded-circle" style="width:40px;height:40px;">
+                <img src="{{asset('storage/avatar/'.($user->avatar??'user_default.jpg'))}}"
+                class="rounded-circle" style="width:40px;height:40px;">
             </td>
             <!-- 編集ボタン -->
             <td>
-            <a href="{{route('profile.edit', $user->id)}}">
-                <button class="btn btn-primary">
-                編集
-                </button>
-            </a>
+                <a href="{{route('profile.edit', $user->id)}}">
+                    <button class="btn btn-primary">
+                    編集
+                    </button>
+                </a>
             </td>
             <!-- 削除ボタン -->
             <td>
-            <form method="post" action="{{route('profile.delete', $user)}}">
-                @csrf
-                @method('delete')
-                <button type="submit" class="btn btn-danger" onClick="return confirm('本当に削除しますか？');">
-                    削除
-                </button>
-            </form>
+                <form method="post" action="{{route('profile.delete', $user)}}">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" class="btn btn-danger" onClick="return confirm('本当に削除しますか？');">
+                        削除
+                    </button>
+                </form>
             </td>
         </tr>
         @endforeach
