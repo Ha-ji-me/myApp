b2568a62964c5225778678a3371ff3f76bd94b98
hajime <hjmx3599@gmail.com>
2022-01-25 18:54:46 +0900

    🚀各メソッドにpaginate追加(home)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,30 +22,38 @@ class HomeController extends Controller
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
+     * 投稿DBの取得とログインユーザーを取得
+     * 新しい順に投稿を表示
+     * ペジネーションで投稿表示
+     *
      */
     public function index()
     {
-        //投稿DBの取得とログインユーザーを取得
-        //新しい順に投稿を表示
-        $incidentPosts=IncidentPost::orderBy('created_at','desc')->get();
+        // $incidentPosts=IncidentPost::orderBy('created_at','desc')->get(); ペじネーションなしver
+        $incidentPosts=IncidentPost::orderBy('created_at','desc')->simplePaginate(10);
         $user=auth()->user();
         return view('home',compact('incidentPosts','user'));
-
-        // return view('home');
     }
 
+    /**
+     * Show the application dashboard.
+     *
+     * 新しい順に表示
+     * ペじネーションで自分の投稿を表示
+     */
     public function myPost()
     {
-        //新しい順に表示
         $user=auth()->user()->id;
-        $incidentPosts=IncidentPost::where('user_id',$user)->orderBy('created_at','desc')->get();
+        // $incidentPosts=IncidentPost::where('user_id',$user)->orderBy('created_at','desc')->get();
+        $incidentPosts=IncidentPost::where('user_id',$user)->orderBy('created_at','desc')->simplePaginate(10);
         return view('myPost',compact('incidentPosts'));
     }
 
     public function myComment()
     {
         $user=auth()->user()->id;
-        $comments=Comment::where('user_id',$user)->orderBy('created_at','desc')->get();
+        // $comments=Comment::where('user_id',$user)->orderBy('created_at','desc')->get();
+        $comments=Comment::where('user_id',$user)->orderBy('created_at','desc')->simplePaginate(10);
         return view('myComment',compact('comments'));
     }
 
