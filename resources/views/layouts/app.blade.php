f6c077a854da5a2fe4960b795c6df64f5919138d
hajime <hjmx3599@gmail.com>
2022-02-13 21:33:15 +0900

    🚀メニューバーを作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'まいあっぷ') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -28,7 +28,7 @@
         <!-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm"> -->
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'まいあっぷ') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -36,9 +36,67 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                    {{-- 2月13日追記箇所 --}}
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <!-- みんなの投稿 -->
+                            みんなの投稿
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('home') }}">
+                                    出来事の投稿
+                                </a>
+                                <!-- ログアウトバー -->
+                                <a class="dropdown-item" href="{{ route('todo-post.index') }}">
+                                    Todoの投稿
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <!-- 投稿の作成 -->
+                            新規投稿
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('incident-post.create') }}">
+                                    出来事の新規投稿
+                                </a>
+                                <a class="dropdown-item" href="{{ route('todo-post.create') }}">
+                                    Todoの新規投稿
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <!-- 自分の投稿 -->
+                            自分の投稿
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('home.myPost') }}">
+                                    出来事の投稿
+                                </a>
+                                <a class="dropdown-item" href="{{ route('home.myPost') }}">
+                                    Todoの投稿
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <!-- 自分の投稿 -->
+                            myアクション
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('home.myComment') }}">
+                                    コメントした投稿
+                                </a>
+                                <a class="dropdown-item" href="{{ route('home.myFavorite') }}">
+                                    お気に入りした投稿
+                                </a>
+                            </div>
+                        </li>
                     </ul>
+                    {{-- ここまで追記 --}}
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -93,7 +151,7 @@
                     </div>
                     <div class="col-12 col-md-8  col-lg-9">
                     <!-- -- エラーメッセージ表示 -- -->
-                    @if ($errors->any())
+                    <!-- @if ($errors->any())
                     <div class="alert alert-danger">
                         <ul>
                             @foreach ($errors->all() as $error)
@@ -101,7 +159,7 @@
                             @endforeach
                         </ul>
                     </div>
-                    @endif
+                    @endif -->
                     @yield('content')
                     </div>
                 </div>
