5dfc3e0329c5c27ffa6ba09159fae59b95551e75
hajime <hjmx3599@gmail.com>
2022-02-18 22:24:00 +0900

    マイグレーションカラム

--- database/migrations/2022_02_09_223929_add_column_is_todo_for_todo_posts_table.php
+++ database/migrations/2022_02_09_223929_add_column_is_todo_for_todo_posts_table.php
@@ -14,7 +14,7 @@ class AddColumnIsTodoForTodoPostsTable extends Migration
     public function up()
     {
         Schema::table('todo_posts', function (Blueprint $table) {
-            $table->boolean('isTodo')->default(0);
+            $table->boolean('is_todo')->default(0);
         });
     }
 
@@ -26,7 +26,7 @@ class AddColumnIsTodoForTodoPostsTable extends Migration
     public function down()
     {
         Schema::table('todo_posts', function (Blueprint $table) {
-            $table->dropColumn('isTodo');
+            $table->dropColumn('is_todo');
         });
     }
 }
