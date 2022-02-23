d6948235aadba6c770d6118d650e01b7d1d4713a
hajime <hjmx3599@gmail.com>
2022-02-23 15:55:44 +0900

    env編集

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'myApp'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'pLSmL5/o_!oX'),
+            'password' => env('DB_PASSWORD', 'Th*0116813599'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
