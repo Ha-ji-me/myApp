565aaaf32f966992d5e14d498ac96dae694699d7
Ha-ji-me <hjmx3599@gmail.com>
2022-02-26 15:15:51 +0900

    fix:🐛authをconstructに追加

--- app/Http/Controllers/IncidentPostController.php
+++ app/Http/Controllers/IncidentPostController.php
@@ -14,6 +14,16 @@ class IncidentPostController extends Controller
     // }
 
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
